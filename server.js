d5c5dc904ee62cf28e9410aec3f7cdc4146bc293
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-03 15:13:58 -0600

    commit

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 // Hello world
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 9750;
+const path = require("path");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(` app listening on port ${PORT}`);
+});
