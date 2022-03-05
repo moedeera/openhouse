ea114ed6c44c907440023b151939ae3ebe9e56f2
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-05 02:47:55 -0600

    commit

--- routes/api/league.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-var cors = require("cors");
-const { Router } = require("express");
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-  res.send("hello");
-});
-module.exports = router;
