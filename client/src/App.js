3d108507b208309d4be2b78c74e9823d2698a360
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-05 17:39:02 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/pages/Main";
+import ProjectOne from "./components/pages/ProjectOne";
+import { ProjectTwo } from "./components/pages/ProjectTwo";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { UserContext } from "./Context/UserContext";
+import { CustomHooks } from "./Context/CustomHooks";
 
 function App() {
+  const { league, teams } = CustomHooks();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-        </a>
-      </header>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Main />} />
+
+            <Route
+              path="/UserContext"
+              element={
+                <UserContext.Provider value={{ league, teams }}>
+                  <ProjectOne />
+                </UserContext.Provider>
+              }
+            />
+
+            <Route path="/Reducers" element={<ProjectTwo />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
