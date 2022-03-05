fc3f9cbc6e66c741ca65d9e82cf4706d76a51bee
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-05 00:46:34 -0600

    commit

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const PORT = process.env.PORT || 9750;
 const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   //Enabling CORS
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+//   );
+//   next();
+// });
 
 app.use("/api/league", require("./routes/api/league"));
 
