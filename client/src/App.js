7dea964f548d9bc0adb16125723a4b5fe7862430
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-05 17:48:18 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Main from "./components/pages/Main";
-import ProjectOne from "./components/pages/ProjectOne";
-import { ProjectTwo } from "./components/pages/ProjectTwo";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { UserContext } from "./Context/UserContext";
-import { CustomHooks } from "./Context/CustomHooks";
 
 function App() {
   return <div className="App">hello</div>;
