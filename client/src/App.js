3957da8c41653cae24c7a9537ee2cfb20464425a
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-05 18:18:52 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/pages/Main";
+import ProjectOne from "./components/pages/ProjectOne";
+import { ProjectTwo } from "./components/pages/ProjectTwo";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { UserContext } from "./Context/UserContext";
+import { CustomHooks } from "./Context/CustomHooks";
 
 function App() {
-  return <div className="App">hello</div>;
+  const { league, teams } = CustomHooks();
+  return (
+    <div className="App">
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Main />} />
+
+            <Route
+              path="/UserContext"
+              element={
+                <UserContext.Provider value={{ league, teams }}>
+                  <ProjectOne />
+                </UserContext.Provider>
+              }
+            />
+
+            <Route path="/Reducers" element={<ProjectTwo />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
